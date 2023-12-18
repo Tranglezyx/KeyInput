7083f3437eaf6017c2876534a41f0f29c8f2e1c3
zhouyunxiang <407833@qq.com>
2023-12-18 17:51:01 +0800

    add

--- demo.py
+++ demo.py
@@ -1,5 +1,7 @@
 import pyautogui
 import win32gui
+import win32api
+import win32con
 import time
 import pygetwindow as gw
 
@@ -7,16 +9,18 @@ def showActiveWindow():
     for window in gw.getAllWindows():
         print(window)
 
+def pressNumber0(window):
+    win32api.PostMessage(window, win32con.WM_KEYDOWN, win32con.VK_NUMPAD0, 0)
+    win32api.PostMessage(window, win32con.WM_KEYUP, win32con.VK_NUMPAD0, 0)
+
 def main():
     # showActiveWindow()
-    windowHandle = pyautogui.getWindowsWithTitle("● 页面接入短信发送.sql - txt - Visual Studio Code")
-    print(windowHandle)
-    if(windowHandle):
-        window = windowHandle[0]
-        print(window)
-        pyautogui.press(window, "A")
-        # 模拟释放 "Enter" 键
-        pyautogui.release(window, "A")
+    # windowHandle = pyautogui.getWindowsWithTitle("页面接入短信发送.sql - txt - Visual Studio Code")
+    window = win32gui.FindWindow(None, "页面接入短信发送.sql - txt - Visual Studio Code")
+    while True:
+        pressNumber0(window)
+        time.sleep(1)
+        
 
 if __name__ == "__main__":
     main()
