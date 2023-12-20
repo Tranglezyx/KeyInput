62443a0cd54f937b45f6cae42857a089dbbeaa7d
zhouyunxiang <407833@qq.com>
2023-12-20 11:34:20 +0800

    add

--- demo.py
+++ demo.py
@@ -1,28 +1,41 @@
-import pyautogui
 import win32gui
 import win32api
 import win32con
 import time
 import pygetwindow as gw
 
-def showActiveWindow():
+
+def showAllWindows():
     for window in gw.getAllWindows():
         print(window)
+
+def showActiveWindow():
+    while True:
+        window = win32gui.GetForegroundWindow()
+        print(window)
+        time.sleep(2)
 
 def pressNumber0(window):
     win32api.PostMessage(window, win32con.WM_KEYDOWN, win32con.VK_F12, 0)
     win32api.PostMessage(window, win32con.WM_KEYUP, win32con.VK_F12, 0)
 
-def click(title):
+def clickWithTitle(title):
     window = win32gui.FindWindow(None, title)
     print("找到的窗口:",window)
     while True:
         pressNumber0(window)
         time.sleep(1)
 
+def clickWithId(id):
+    while True:
+        pressNumber0(id)
+        time.sleep(1)
+
 def main():
-    # showActiveWindow()
-    click("阿里云控制台-日志服务 - Google Chrome")
+    # showAllWindows()
+    # clickWithTitle("阿里云控制台-日志服务 - Google Chrome")
+    showActiveWindow()
+    clickWithId(198096)
         
 
 if __name__ == "__main__":
