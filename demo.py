a5fec08974f995e1f93701205e09471e01338d4f
zhouyunxiang <407833@qq.com>
2023-12-20 11:24:27 +0800

    add

--- demo.py
+++ demo.py
@@ -10,16 +10,19 @@ def showActiveWindow():
         print(window)
 
 def pressNumber0(window):
-    win32api.PostMessage(window, win32con.WM_KEYDOWN, win32con.VK_NUMPAD0, 0)
-    win32api.PostMessage(window, win32con.WM_KEYUP, win32con.VK_NUMPAD0, 0)
+    win32api.PostMessage(window, win32con.WM_KEYDOWN, win32con.VK_F12, 0)
+    win32api.PostMessage(window, win32con.WM_KEYUP, win32con.VK_F12, 0)
 
-def main():
-    # showActiveWindow()
-    # windowHandle = pyautogui.getWindowsWithTitle("页面接入短信发送.sql - txt - Visual Studio Code")
-    window = win32gui.FindWindow(None, "页面接入短信发送.sql - txt - Visual Studio Code")
+def click(title):
+    window = win32gui.FindWindow(None, title)
+    print("找到的窗口:",window)
     while True:
         pressNumber0(window)
         time.sleep(1)
+
+def main():
+    # showActiveWindow()
+    click("阿里云控制台-日志服务 - Google Chrome")
         
 
 if __name__ == "__main__":
