25ecb15bdb29b75b041ac610563d5782751c5ab1
zhouyunxiang <407833@qq.com>
2023-12-29 11:05:19 +0800

    add

--- demo.py
+++ demo.py
@@ -1,24 +1,37 @@
 import time
 import keyboard
+import threading
 
+pause_event = threading.Event()
 
-def pressStartListener(event):
+def pressListener(event):
     key = event.name
     if key == ',':
         print("开始")
-        while True:
-            keyboard.press('a')
-            time.sleep(2)
-
-def pressEndListener(event):
-    key = event.name
+        pause_event.set()
+        threadA.start()
+        threadB.start()
     if key == '.':
         print("停止")
-        keyboard.unhook_all()
+        pause_event.clear()
+
+def newThreadPressA():
+    while True:
+        pause_event.wait()
+        keyboard.press('a')
+        time.sleep(2)
+
+def newThreadPressB():
+    while True:
+        pause_event.wait()
+        keyboard.press('b')
+        time.sleep(600)
+
+threadA = threading.Thread(target=newThreadPressA) 
+threadB = threading.Thread(target=newThreadPressB) 
 
 def main():
-    keyboard.on_press(pressStartListener)
-    keyboard.on_press(pressEndListener)
+    keyboard.on_press(pressListener)
     keyboard.wait()
 
 if __name__ == "__main__":
