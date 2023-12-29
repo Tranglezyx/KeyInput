f8d1bcf0a36fd1b69c90d187f35eaab966a6bc8f
zhouyunxiang <407833@qq.com>
2023-12-29 10:11:11 +0800

    add

--- demo.py
+++ demo.py
@@ -1,67 +1,25 @@
-import win32gui
-import win32api
-import win32con
 import time
-import pygetwindow as gw
-import pyautogui
-import ctypes
+import keyboard
 
-def showAllWindows():
-    for window in gw.getAllWindows():
-        print(window)
 
-def showActiveWindow():
-    while True:
-        window = win32gui.GetForegroundWindow()
-        print(window)
-        time.sleep(2.5)
+def pressStartListener(event):
+    key = event.name
+    if key == ',':
+        print("开始")
+        while True:
+            keyboard.press('a')
+            time.sleep(2)
 
-def pressNumber0(window):
-    win32api.PostMessage(window, win32con.WM_KEYDOWN, win32con.VK_F12, 0)
-    win32api.PostMessage(window, win32con.WM_KEYUP, win32con.VK_F12, 0)
-
-def clickWithTitle(title):
-    window = win32gui.FindWindow(None, title)
-    print("找到的窗口:",window)
-    while True:
-        pressNumber0(window)
-        time.sleep(1)
-
-def clickWithId(id):
-    while True:
-        pressNumber0(id)
-        time.sleep(1)
-
-def clickA(inteval):
-    while True:
-        # pyautogui.press("A")
-        pyautogui.hotkey("alt","a")
-        time.sleep(inteval)
-        pyautogui.press("esc")
-        time.sleep(inteval)
-
-user32 = ctypes.WinDLL('user32')
-VK_A = 0x41
-# 定义键盘按键标志常量
-KEYEVENTF_KEYDOWN = 0x0000
-KEYEVENTF_KEYUP = 0x0002
-
-def clickA1(inteval):
-    while True:
-        # 加载user32.dll库
-        # 调用keybd_event函数模拟按键按下
-        user32.keybd_event(65, 0, KEYEVENTF_KEYDOWN, 0)
-        
-        # 调用keybd_event函数模拟按键释放
-        user32.keybd_event(65, 0, KEYEVENTF_KEYUP, 0)
-        time.sleep(1)
+def pressEndListener(event):
+    key = event.name
+    if key == '.':
+        print("停止")
+        keyboard.unhook_all()
 
 def main():
-    # showAllWindows()
-    # clickWithTitle("阿里云控制台-日志服务 - GAoogle Chrome")AAA
-    # showActiveWindow()
-    # clickWithId(198096)
-    clickA1(1)
+    keyboard.on_press(pressStartListener)
+    keyboard.on_press(pressEndListener)
+    keyboard.wait()
 
 if __name__ == "__main__":
     main()
--- key.lua
+++
@@ -1,5 +0,0 @@
-while true do  
-    os.execute("keydown " .. "A")  
-    os.execute("key up " .. "A")  
-    os.execute("sleep 1")  
-end
